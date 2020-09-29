b5a0485323a941fd0aefaa43d83c8156687b5a07
rozalen <rozalen95@gmail.com>
2020-09-29 23:17:36 +0200

    add assets and init vuetify custom theme

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#E85C40',
+        secondary: '#303E47',
+        accent: '#FF783B',
+        error: '#FF3F3B',
+      },
+    },
+  },
 });
